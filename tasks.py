9401bffbe522179f5e337c69fa2b6f89b0b42ba1
chicacode <gerahernan.al@gmail.com>
2025-02-18 12:46:34 -0800

    Added view tasks functionality

--- tasks.py
+++ tasks.py
@@ -1,3 +1,12 @@
 def add_task(task_list, task):
     task_list.append(task)
     print(f'{task} has been added to the list')
+
+
+def view_tasks(task_list):
+    if not task_list:
+        print('No tasks in the To-Do List')
+    else:
+        print('\n To-Do List Tasks')
+        for index, task in enumerate(task_list, 1):
+            print(f'{index}. {task}')
